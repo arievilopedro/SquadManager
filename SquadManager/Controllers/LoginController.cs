5967b263a1b5f6c8b86034629b34704268691233
Pedro Oliveira <arievilopedro@gmail.com>
2022-12-01 17:10:17 +0000

    Criação tela de Login com CSS/HTML - Iniciando no MVC

---
+++ SquadManager/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SquadManager.Models;
+
+namespace SquadManager.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserViewModel user = new UserViewModel();
+
+            return View("Index", user);
+        }
+
+        [HttpPost]
+        public IActionResult Test(UserViewModel user)
+        {
+            user.Email = "email enviado";
+
+            return View("Index", user);
+        }
+    }
+}
